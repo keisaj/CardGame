1c4fe0f32274ddda008e77f7de9af1f7bb408af1
Jan Bukała <janbukala96@gmail.com>
2022-06-21 10:27:45 +0200

    minor fixes

--- CardGame/main.py
+++ CardGame/main.py
@@ -40,7 +40,6 @@ class RandomPlayer(Player):
     def set_final_reward(self, points: dict):
         pass
 
-<<<<<<< HEAD
 class DebugPlayer(RandomPlayer):
     """
     Makes random moves (but according to the rules)
@@ -62,8 +61,6 @@ class DebugPlayer(RandomPlayer):
     def set_final_reward(self, points: dict):
         return super().set_final_reward(points)
 
-=======
->>>>>>> 775a9d215b0b00de82939534141a437dc5137d8a
 class GambitPlayer(Player):
 
     name = 'Gambit Player'
@@ -71,7 +68,6 @@ class GambitPlayer(Player):
         global player
         self.number = player
         player += 1
-<<<<<<< HEAD
         
     def make_move(self, game_state: dict) -> Card:
         """
@@ -82,17 +78,10 @@ class GambitPlayer(Player):
         played_card = super().make_move(game_state)
         return played_card
 
-=======
-
-    def make_move(self, game_state: dict) -> Card:
-        return super().make_move(game_state)
->>>>>>> 775a9d215b0b00de82939534141a437dc5137d8a
-
     def get_name(self) -> str:
         return self.name + f'{self.number}'
     
     def set_temp_reward(self, discarded_cards: dict, point_deltas: dict):
-<<<<<<< HEAD
         """
         After four cards get played, every player will receive a dict with:
         - 'discarded_cards': dict of these discarded cards by each player (ordered!)
@@ -105,13 +94,7 @@ class GambitPlayer(Player):
         After all cards have been played, every player will receive a dict with
         points received in this round by each player. A game consists of eleven such full rounds.
         """
-=======
         return super().set_temp_reward(discarded_cards, point_deltas)
-    
-    def set_final_reward(self, points: dict):
->>>>>>> 775a9d215b0b00de82939534141a437dc5137d8a
-        return super().set_final_reward(points)
-
 
 def main():
     game = CardGame(RandomPlayer(), RandomPlayer(), DebugPlayer(), RandomPlayer(), delay=100, display=True, full_deck=False)
