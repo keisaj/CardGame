b0c0bfc405bed2cd0ba4d2d4bf9dfcd606ddbc82
Jan Bukała <janbukala96@gmail.com>
2022-06-21 10:21:55 +0200

    debug player added

--- CardGame/main.py
+++ CardGame/main.py
@@ -40,11 +40,68 @@ class RandomPlayer(Player):
     def set_final_reward(self, points: dict):
         pass
 
+class DebugPlayer(RandomPlayer):
+    """
+    Makes random moves (but according to the rules)
+    """
+    def __init__(self):
+        global player
+        self.number = player
+        player += 1
+
+    def make_move(self, game_state: dict) -> Card:
+        return super().make_move(game_state)
+
+    def get_name(self):
+        return f"DebugPlayer{self.number}"
+
+    def set_temp_reward(self, discarded_cards: dict, point_deltas: dict):
+        return super().set_temp_reward(discarded_cards, point_deltas)
+
+    def set_final_reward(self, points: dict):
+        return super().set_final_reward(points)
+
+class GambitPlayer(Player):
+
+    name = 'Gambit Player'
+    def __init__(self) -> None:
+        global player
+        self.number = player
+        player += 1
+        
+    def make_move(self, game_state: dict) -> Card:
+        """
+        The player will receive a dict with:
+        - 'hand': list of held cards
+        - 'discard': list of discarded cards in this round
+        """
+        played_card = super().make_move(game_state)
+        return played_card
+
+
+    def get_name(self) -> str:
+        return self.name + f'{self.number}'
+    
+    def set_temp_reward(self, discarded_cards: dict, point_deltas: dict):
+        """
+        After four cards get played, every player will receive a dict with:
+        - 'discarded_cards': dict of these discarded cards by each player (ordered!)
+        - 'point_deltas': dict of points received in this round by each player
+        """
+        return super().set_temp_reward(discarded_cards, point_deltas)
+    
+    def set_final_reward(self, points: dict):
+        """
+        After all cards have been played, every player will receive a dict with
+        points received in this round by each player. A game consists of eleven such full rounds.
+        """
+        return super().set_final_reward(points)
+
 
 def main():
-    game = CardGame(RandomPlayer(), RandomPlayer(), RandomPlayer(), RandomPlayer(), delay=100, display=False)
+    game = CardGame(RandomPlayer(), RandomPlayer(), DebugPlayer(), RandomPlayer(), delay=100, display=True, full_deck=False)
     print(game.start())
-    print(game.start())
+    # print(game.start())
 
 
 if __name__ == '__main__':
