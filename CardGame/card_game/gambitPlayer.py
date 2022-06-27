0a756abccdb41d3d6455ea9567e65ac908cb1b5a
Jan Bukała <janbukala96@gmail.com>
2022-06-27 19:31:03 +0200

    gambitPlayer developed

--- CardGame/card_game/gambitPlayer.py
+++ CardGame/card_game/gambitPlayer.py
@@ -1,5 +1,8 @@
 # from player import Player
 from card_game import CardGame, Player, Card
+import random
+import numpy as np
+from tensorflow import keras
 
 class GambitPlayer(Player):
 
@@ -8,6 +11,8 @@ class GambitPlayer(Player):
         global player
         self.number = player
         player += 1
+        self.epsilon = 1.0 
+        self.model = self.create_model()
         
     def make_move(self, game_state: dict) -> Card:
         """
@@ -39,4 +44,41 @@ class GambitPlayer(Player):
         points received in this round by each player. A game consists of eleven such full rounds.
         """
         return super().set_final_reward(points)
+
+    def get_action(self, state):
+        """
+        Compute the action to take in the current state, including exploration.
+        With probability self.epsilon, we should take a random action.
+            otherwise - the best policy action (self.get_best_action).
+
+        Note: To pick randomly from a list, use random.choice(list).
+              To pick True or False with a given probablity, generate uniform number in [0, 1]
+              and compare it with your probability
+        """
+        possible_actions = self.get_legal_actions(state)       
+
+        return random.choice(possible_actions) if (np.random.random() <= self.epsilon) else self.get_best_action(state)
+
+    def get_best_action(self, state):
+        """
+        Compute the best action to take in a state.
+        """
+        return np.argmax(self.model.predict(state)[0])
+    
+    def get_legal_actions(self, state):
+        return state['hand']
+
+    def create_model(self):
+        learning_rate = 0.001
+        action_size = 6 # 6 kart dostajemy na start [1, 2, 3, 4, 5, 0,| 5, 6, 7, 8 ]
+
+        model = keras.Sequential()
+        model.add(keras.layers.Dense(64, input_dim=state_size, activation='relu'))
+        model.add(keras.layers.Dense(64, activation="relu"))
+        model.add(keras.layers.Dense(action_size))
+        model.compile(loss="mse",
+              optimizer=keras.optimizers.Adam(learning_rate=learning_rate))
+
+
+
 
